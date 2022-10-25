e39987fe2b4b890fad1e615aff0e9d1bcf6035fe
JuanSilva <juan_nascimento@alunos.unicesumar.edu.br>
2022-10-25 20:34:57 -0300

    inserção do projeto telaIntegrador2022

---
+++ telaIntegrador2022/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace telaIntegrador2022
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void Desativar_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Executa_Int_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Loop_btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void att_Banco_bnt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
